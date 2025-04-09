89af54419def2cd3297c847c0b173abdd1a7d9b2
ArvindRaghav56 <arvindraghavmn@gmail.com>
2025-04-09 13:04:14 +0530

    Delete feedback_entry.py

--- feedback_entry.py
+++
@@ -1,8 +0,0 @@
-def collect_feedback(student_name, course, rating, comments=""):
-    """Collect student feedback"""
-    return {
-        "student_name": student_name,
-        "course": course,
-        "rating": rating,
-        "comments": comments
-    }
